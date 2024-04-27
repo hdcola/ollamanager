142fa02b71e831dd0ab4f0316a5c5fcb06097572
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2024-04-27 11:12:20 -0400

    add

---
+++ ChatOllama/ChatOllama/View/ChatView.swift
@@ -0,0 +1,67 @@
+//
+//  ChatView.swift
+//  ChatOllama
+//
+//  Created by Sicheng Jiang on 2024-04-27.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    @State var vm = ChatViewModel()
+    @State var mvm = MessageViewModel()
+    @AppStorage("selectedModel") var selectedModel = ""
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State var message = ""
+    var body: some View {
+        VStack {
+            MessageView(lm: mvm.lastMessages)
+            Spacer()
+            HStack {
+                TextField("Message", text: $message)
+                    .textFieldStyle(.roundedBorder)
+                    .disableAutocorrection(true)
+                    .disabled(!mvm.isCompleted)
+                    .onSubmit {
+                        sendMessage()
+                    }
+                Button {
+                    sendMessage()
+                } label: {
+                    Image(systemName:mvm.isCompleted ? "arrow.up" : "stop.fill")
+                        .font(.title)
+                }
+                .disabled(message.isEmpty && mvm.isCompleted)
+            }
+            HStack {
+                Text("Status:")
+                Image(systemName: "circle.fill")
+                    .foregroundColor(vm.reachable ? .green : .red)
+                Picker("Model",selection: $vm.selectedModel){
+                    Text("Select a model")
+                    ForEach(vm.availModels, id: \.self.name) {
+                        Text($0.name)
+                    }
+                }
+                .pickerStyle(.menu)
+                .padding()
+                .onReceive(timer) { _ in
+                    Task {
+                        await vm.checkReachable()
+                        await vm.getModels()
+                    }
+                }
+                
+            }
+        }
+    }
+    func sendMessage() {
+        mvm.sendButton(sendingMessage: message, usedModel: vm.selectedModel)
+        message = ""
+    }
+}
+
+#Preview {
+    ChatView()
+        .frame(minWidth: 200, minHeight: 400)
+}
